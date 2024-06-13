a278c888c09bd5523138519a034458da2e812194
Moumouni BELEM <moumouni.belem@cm-robotics.com>
2024-06-13 18:18:01 +0200

    feat: implement smoother controller for wheeld robots
    header file

---
+++ diff_wheeled_smooth_ctrl/include/diff_wheeled_smooth_ctrl/smooth_ctrl.hpp
@@ -0,0 +1,35 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+
+
+class  SmoothCtrl: public rclcpp::Node
+{
+  
+  public:
+    SmoothCtrl(const std::string& name);
+
+  private:
+    float k1_;
+    float k2_;
+    float beta_;
+    float lambda_;
+    float v_max_;
+    bool active_ = false;
+    geometry_msgs::msg::Pose2D home_pose_;
+    geometry_msgs::msg::Pose2D current_pose_;
+
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
+    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_subscriber_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_subscriber_;
+    std::shared_ptr<rclcpp::TimerBase> cmd_vel_publisher_timer_;
+
+    geometry_msgs::msg::Pose2D pose_relative(geometry_msgs::msg::Pose2D &home_pose, geometry_msgs::msg::Pose2D &pose);
+
+    void pose_subscriber_callback(const geometry_msgs::msg::PoseWithCovarianceStamped & msg);
+    void goal_subscriber_callback(const geometry_msgs::msg::PoseStamped & msg);
+    void cmd_vel_publisher_timer_callback();
+    float mod_angle(float angle);
+};
